4a0cbb4a13f57de5b53ae1226f678f745553cf5b
jesuscirino <jesuscirino@gmail.com>
2019-10-01 13:15:54 -0500

    enc-man

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const PDF = require("pdfkit");
 const fs = require("fs");
 const { destination, _margins } = require("./config.json");
 import { cmToPoints } from "./lib/m1.js";
-
+// set margins
 const marginsPoints = cmToPoints(_margins);
 
 const doc = new PDF({ layout: "landscape", margins: marginsPoints });
 doc.pipe(fs.createWriteStream(destination));
-console.log(doc.page.width);
-console.log(doc.page.height);
-doc.moveTo(0, 0);
-doc.rect(0, 0, doc.page.width, 72);
-doc.stroke();
-doc.text("SOIX");
+console.log(`Width=${doc.page.width} - Height=${doc.page.height}`);
+//doc.moveTo(10, 10);
+doc
+  .roundedRect(5, 5, doc.page.width - 10, 72, 8)
+  .fillOpacity(0.5)
+  .fillAndStroke("#FEA500", "#FEA500")
+  .text("SOIX", 50, 50);
+doc.moveTo(10, 10);
+doc.text("SOIX", 50, 50);
 doc.end();
