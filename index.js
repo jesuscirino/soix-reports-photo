f528f90d624bd4597625628a5a3f0a3932b7a062
jesuscirino <jesuscirino@gmail.com>
2019-10-04 16:07:44 -0500

    previous to image insertions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const PDF = require("pdfkit");
+const sizeOf = require("image-size");
 const fs = require("fs");
 const {
   destination,
@@ -7,60 +8,64 @@ const {
   header,
   info
 } = require("./config/config.json");
-import { cmToPoints } from "./lib/m1.js";
+import * as trace from "./lib/m1.js";
 
 // set margins
-const marginsPoints = cmToPoints(_margins);
+const marginsPoints = trace.cmToPoints(_margins);
 
 const doc = new PDF({ layout: "landscape", margins: marginsPoints });
 const { height: HEIGHT, width: WIDTH } = doc.page;
 doc.pipe(fs.createWriteStream(destination));
-console.log(`Width=${WIDTH} - Height=${WIDTH}`);
-const [xCol1, yCol1, widthCol1, heightCol1, cornerRad, gap] = [
+
+console.log(`Width=${WIDTH} - Height=${HEIGHT}`);
+
+const [xCol1, yCol1, widthCol1, heightCol1, cornerRad] = [
   14,
   65,
   WIDTH / 7,
   48,
-  8,
-  5
+  8
 ];
+const yCanvas = yCol1 + heightCol1 + 10;
+const hCanvas = HEIGHT - (35 + yCanvas + 10);
+const xCanvas = WIDTH - (hCanvas + 14);
 
-doc.image(header, 0, 0, { width: WIDTH });
-doc.image(footer, xCol1, HEIGHT - 35, { width: WIDTH - 28 });
+/*trace.header(doc, header, 0, 0, WIDTH);
+trace.footer(doc, footer, xCol1, HEIGHT, WIDTH);
+trace.newCanvas(doc, xCanvas, yCanvas, hCanvas, cornerRad);
+trace.tableInfo(
+  doc,
+  xCol1,
+  yCol1,
+  widthCol1 - 5,
+  heightCol1,
+  cornerRad,
+  info,
+  WIDTH
+);
+*/
+trace.coverPage(
+  doc,
+  header,
+  footer,
+  xCanvas,
+  yCanvas,
+  xCol1,
+  yCol1,
+  widthCol1,
+  heightCol1,
+  hCanvas,
+  cornerRad,
+  info
+);
 
-doc
-  .roundedRect(xCol1, yCol1, widthCol1 - 5, heightCol1, cornerRad)
-  .fillAndStroke("#FEA500", "#FEA500");
-
-doc
-  .fillOpacity(1)
-  .fillColor("#FFFFFF")
-  .font("Helvetica-Bold")
-  .fontSize(10)
-  .text(info.cel11, xCol1 + gap, yCol1 + gap)
-  .moveDown(0.2)
-  .text(info.cel21)
-  .moveDown(0.2)
-  .text(info.cel31);
-
-doc
-  .roundedRect(
-    xCol1 + WIDTH / 7,
-    yCol1,
-    WIDTH - (WIDTH / 7 + 28),
-    heightCol1,
-    cornerRad
-  )
-  .fillAndStroke();
-doc
-  .fillColor("#3D3E3E")
-  .font("Helvetica-Bold")
-  .fontSize(10)
-  .text(info.cel12, xCol1 + gap + WIDTH / 7, yCol1 + gap)
-  .moveDown(0.2)
-  .font("Helvetica")
-  .text(info.cel22)
-  .moveDown(0.2)
-  .text(info.cel32);
-
+doc.on("pageAdded", () => {
+  trace.header(doc, header, 0, 0, WIDTH);
+  trace.footer(doc, footer, xCol1, HEIGHT, WIDTH);
+  trace.newCanvas(doc, 14, yCol1, WIDTH - 28, HEIGHT - (yCol1 + 45), cornerRad);
+});
+doc.addPage();
+const r = trace.newRect(doc, 50, 50, 100, 100);
+r.trace("#fffff");
+doc.addPage();
 doc.end();
